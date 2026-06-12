144def3bac0478dda86d2f04c024b01441f81849
Raj280901 <103771707+Raj280901@users.noreply.github.com>
2026-06-12 19:17:52 +0530

    Update dash.py

--- dash.py
+++ dash.py
@@ -18,42 +18,78 @@ def upload_file():
     else:
         st.info("Please upload an excel file to display the data")
 
-        
 def data_preview(df):
     try:
         s_names = list(df.keys())
         st.subheader("Data Preview")
+        # if "selected_sheet" not in st.session_state:
+        #     st.session_state.selected_sheet = s_names[0]
 
-        if "selected_sheet" not in st.session_state:
-            st.session_state.selected_sheet = s_names[0]
+        # current_df = df[st.session_state.selected_sheet]
 
-        current_df = df[st.session_state.selected_sheet]
+        # st.dataframe(current_df, width='stretch', height=500)
+        # selected_sheet = option_menu(
+        #     menu_title=None,
+        #     options=s_names,
+        #     icons=["file-earmark-spreadsheet"] * len(s_names),
+        #     menu_icon="cast",
+        #     default_index=s_names.index(st.session_state.selected_sheet),
+        #     orientation="horizontal",
+        # )
+        # if selected_sheet != st.session_state.selected_sheet:
+        #     st.session_state.selected_sheet = selected_sheet
+        #     st.rerun()
 
-        st.dataframe(current_df, width='stretch', height=500)
-        selected_sheet = option_menu(
-            menu_title=None,
-            options=s_names,
-            icons=["file-earmark-spreadsheet"] * len(s_names),
-            menu_icon="cast",
-            default_index=s_names.index(st.session_state.selected_sheet),
-            orientation="horizontal",
-        )
-        if selected_sheet != st.session_state.selected_sheet:
-            st.session_state.selected_sheet = selected_sheet
-            st.rerun()
-
+        tabu = st.tabs(s_names)
+        for i, tab in enumerate(tabu):
+            with tab:
+                st.dataframe(df[s_names[i]], width='stretch')
+        
         return s_names
 
     except Exception as e:
         st.error("Error processing the excel - {}".format(e))
 
-def topFive_from_each_batch(df, s_names):
+def display_top_n(top_n_d, s_names):
+    st.subheader("Top 5 from each Batch")
+    tabs = st.tabs(s_names)
+    for i, tab in enumerate(tabs):
+        with tab:
+            st.dataframe(top_n_d[s_names[i]], width='stretch')
+
+def display_bottom_n(bottom_n_d, s_names):
+    st.subheader("Bottom 5 from each Batch")
+    tabs = st.tabs(s_names)
+    for i, tab in enumerate(tabs):
+        with tab:
+            st.dataframe(bottom_n_d[s_names[i]], width='stretch')
+
+def select_n(df, s_names):
+    max_stu = 0
+    for y in range(len(s_names)):
+        max_stu = max(max_stu, len(df[s_names[y]])) #max student for dropdown
+    st.subheader("Enter the number of students to display in Merit list ->")
+
+    n = st.selectbox(
+        label="",
+        options=list(range(5, max_stu+1, 5)),
+        index=None, #default top - 5
+        label_visibility="collapsed"
+    )
+
+    return n
+
+def top_n_from_each_batch(df, s_names, n):
     try:
         top_from_each_batch = []  #for bar chart
         all_student_percentage = [] #pie chart for all student
 
-        cols = st.columns(2)
+        # cols = st.columns(2)
+        
+        top_n_d = {}
+        bottom_n_d = {}
         # print(len(s_names))
+        
         for x in range(len(s_names)):
             # print(x)
             dfx = df[s_names[x]]
@@ -63,16 +99,22 @@ def topFive_from_each_batch(df, s_names):
                 dfx["Percentage"] = dfx["Percentage"].round(2)
                 all_student_percentage.extend(dfx["Percentage"].tolist())
 
-                top_5 = dfx.sort_values(by="TotalMarks", ascending=False).head(5)[["Student Name", "TotalMarks"]].reset_index(drop=True)
+                sorted_val = dfx.sort_values(by="TotalMarks", ascending=False)
 
-                with cols[x%(len(cols))]:
-                    st.subheader("Top 5 from {}".format(s_names[x]))
-                    # st.table(top_5.reset_index(drop=True))
-                    st.dataframe(
-                        top_5,
-                        width='stretch',
-                        hide_index=True
-                    )
+                top_5 = sorted_val.head(n)[["Student Name", "TotalMarks"]].reset_index(drop=True)
+                bottom_5 = sorted_val.tail(n)[["Student Name", "TotalMarks"]].iloc[::-1].reset_index(drop=True)
+
+                top_n_d[s_names[x]] = top_5
+                bottom_n_d[s_names[x]] = bottom_5
+                
+                # with cols[x%(len(cols))]:
+                #     st.subheader("Top 5 from {}".format(s_names[x]))
+                #     # st.table(top_5.reset_index(drop=True))
+                #     st.dataframe(
+                #         top_5,
+                #         width='stretch',
+                #         hide_index=True
+                #     )
                 top_student = top_5.iloc[0]
                 top_from_each_batch.append({
                     "Batch": s_names[x],
@@ -81,11 +123,16 @@ def topFive_from_each_batch(df, s_names):
                 })
 
             else:
-                st.warning("Sheet {} must contain 'Name' and 'Marks' columns".format(df.keys()[x]))
-        return top_from_each_batch, all_student_percentage
+                st.warning("Sheet {} must contain 'Name' and 'Marks' columns".format(s_names[x]))
+          
+        # return top_from_each_batch, all_student_percentage
+        display_top_n(top_n_d, s_names)
+        display_bottom_n(bottom_n_d, s_names)
 
     except Exception as e:
         st.error("Error processing the excel - {}".format(e))
+    
+    return top_from_each_batch, all_student_percentage
 
 def topper_comparison_from_each_batch(top_from_each_batch):
 
@@ -179,7 +226,7 @@ def phy_subject_difficulty(df, s_names):
             })
 
     summary_df = pd.DataFrame(all_batches_phy_data)
-    print(summary_df)
+    # print(summary_df)
     summary_df = summary_df.sort_values(by="Average Physics Score")
 
     # col1, col2 = st.columns([1,2])
@@ -195,7 +242,7 @@ def phy_subject_difficulty(df, s_names):
     # with col2:
     st.subheader("Visual Comparison for Physics")
 
-    fig_b = px.bar(
+    fig_p = px.bar(
         summary_df,
         x="Batch",
         y="Average Physics Score",
@@ -205,7 +252,7 @@ def phy_subject_difficulty(df, s_names):
         color_continuous_scale="Reds_r"
     )
 
-    st.plotly_chart(fig_b, width='stretch')
+    st.plotly_chart(fig_p, width='stretch', key="phy")
 
 def chem_subject_difficulty(df, s_names):
     all_batches_chem_data = []
@@ -223,7 +270,7 @@ def chem_subject_difficulty(df, s_names):
             })
 
     summary_df = pd.DataFrame(all_batches_chem_data)
-    print(summary_df)
+    # print(summary_df)
     summary_df = summary_df.sort_values(by="Average Chemistry Score")
 
     # col1, col2 = st.columns([1,2])
@@ -239,7 +286,7 @@ def chem_subject_difficulty(df, s_names):
     # with col2:
     st.subheader("Visual Comparison for Chemistry")
 
-    fig_b = px.bar(
+    fig_c = px.bar(
         summary_df,
         x="Batch",
         y="Average Chemistry Score",
@@ -249,7 +296,7 @@ def chem_subject_difficulty(df, s_names):
         color_continuous_scale="Reds_r"
     )
 
-    st.plotly_chart(fig_b, width='stretch')
+    st.plotly_chart(fig_c, width='stretch', key="chem")
 
 def math_subject_difficulty(df, s_names):
     all_batches_math_data = []
@@ -267,7 +314,7 @@ def math_subject_difficulty(df, s_names):
             })
 
     summary_df = pd.DataFrame(all_batches_math_data)
-    print(summary_df)
+    # print(summary_df)
     summary_df = summary_df.sort_values(by="Average Maths Score")
 
     # col1, col2 = st.columns([1,2])
@@ -283,7 +330,7 @@ def math_subject_difficulty(df, s_names):
     # with col2:
     st.subheader("Visual Comparison for Maths")
 
-    fig_b = px.bar(
+    fig_m = px.bar(
         summary_df,
         x="Batch",
         y="Average Maths Score",
@@ -293,7 +340,7 @@ def math_subject_difficulty(df, s_names):
         color_continuous_scale="Reds_r"
     )
 
-    st.plotly_chart(fig_b, width='stretch')
+    st.plotly_chart(fig_m, width='stretch', key="math")
 
 def download_file():
     url1 = "https://github.com/Raj280901/dashboard/raw/refs/heads/main/StudentData_File1.xlsx"
@@ -304,7 +351,19 @@ def download_file():
         st.error("Failed to fetch demo file")
         return None
 
+def visual_tabs(df, s_names):
+    st.subheader("Subjectwise Summary table & Visual Comparison")
+    tab1, tab2, tab3 = st.tabs(["Physics", "Chemistry", "Maths"])
+    with tab1:
+        phy_subject_difficulty(df, s_names)
+    with tab2:
+        chem_subject_difficulty(df, s_names)
+    with tab3:
+        math_subject_difficulty(df, s_names)
+
 st.write("Hello")
+
+st.write("***In case you don't have a file to test ->***")
 # path = "C:\\Users\\Raj Gandhi\\Downloads\\output\\data.xlsx"
 
 file_data = download_file()
@@ -315,13 +374,16 @@ if file_data:
         file_name="Student_data_demo.xlsx",
         mime="text/xlsx"
     )
-
 df = upload_file()
 if df:
     s_names = data_preview(df)
-    top_from_each_batch, all_student_percentage = topFive_from_each_batch(df, s_names)
-    topper_comparison_from_each_batch(top_from_each_batch)
-    performance_cateories(all_student_percentage)
-    phy_subject_difficulty(df, s_names)
-    chem_subject_difficulty(df, s_names)
-    math_subject_difficulty(df, s_names)
+    n = select_n(df, s_names)
+    if n:
+        top_from_each_batch, all_student_percentage = top_n_from_each_batch(df, s_names, n)
+        topper_comparison_from_each_batch(top_from_each_batch)
+        performance_cateories(all_student_percentage)
+        visual_tabs(df, s_names)
+    else:
+        st.info("Please select top n students")
+
+    
